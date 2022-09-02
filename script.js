460f91d868c09f3fe83f34f489ecc5e165fe6f7e
lucasalmeida30 <lucas_almeida12@icloud.com>
2022-09-01 22:42:52 -0300

    alguns requisitos finalizados

--- script.js
+++ script.js
@@ -1,31 +1,45 @@
-// função para as 4 primeiras cores da paleta
-
+// As 4 primeiras cores da paleta
+// let corPreta = document.querySelector('.selected')
 let corPaleta = document.getElementsByClassName('color')
-corPaleta[0].style.backgroundColor = 'black'
+// corPreta.style.backgroundColor = 'black'
 corPaleta[1].style.backgroundColor = 'blue'
 corPaleta[2].style.backgroundColor = 'red'
 corPaleta[3].style.backgroundColor = 'green'
 
-function generateColor() {
+// Função para gerar cor aleatorio
+function gerarCorAleatoria() {
     const letters = '0123456789ABCDEF';
     let color = '#';
-    
     for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
+        color += letters[Math.floor(Math.random() * 16)];
     }
-    
     return color;
-    
-  }
-
-
-let button = document.getElementById('button-random-color')
-button.addEventListener('click', gerarCorAleatoria)
-function gerarCorAleatoria() {
-    for(let index = 1; index < corPaleta.length; index += 1){
-        corPaleta[index].style.backgroundColor = generateColor()
-    }
 }
+
+// função do click do botão
+let button = document.getElementById('button-random-color')
+button.addEventListener('click', gerarPaleta)
+
+function gerarPaleta() {
+    let array = [];
+    for (let index = 1; index < corPaleta.length; index += 1) {
+        let corAleatorio = corPaleta[index].style.backgroundColor = gerarCorAleatoria()
+        array.push(corAleatorio);
+    }
+    localStorage.clear()
+    const coresString = JSON.stringify(array)
+    localStorage.setItem('colorPalette', coresString)
+    console.log(coresString)
+}
+
+
+
+
+
+
+
+
+
 
 
 
